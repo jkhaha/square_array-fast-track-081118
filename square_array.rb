d4f225941dae3825729977731800dada811905dc
Jackie Ha <jha2@wellesley.edu>
2018-08-12 18:30:24 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,8 @@
 def square_array(array)
-  return_array = []
-  array.each do |num| 
-    # and square it....
-    # puts "#{number}**= number"
-  return_array.push num*num
-    # add the result to the return array
+  array.map do |num| 
+
+  num*num
   end
-  return_array
+
+
 end
