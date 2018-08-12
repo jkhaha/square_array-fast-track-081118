ff68822a089eaf12f13e1a13a469c6829aac2dfd
Jackie Ha <jha2@wellesley.edu>
2018-08-12 18:16:03 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,8 @@
 def square_array(array)
-  array.each do |numbers|
+  array.each do |numbers| 
+    # take each number from the array of numbers
     puts "#{number}**= number"
+    # and square it....
   end
 end
 
